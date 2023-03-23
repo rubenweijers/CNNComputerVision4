c0f9dac14c96fa58b5e50e15449d75bb2809e0d2
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-03-23 13:46:21 +0100

    Fixed custom object loading/saving

--- evaluate.py
+++ evaluate.py
@@ -39,17 +39,17 @@ def plot_learning_rate_schedule(epochs: 15, learning_rate: 0.001, batch_size: in
 
 
 if __name__ == "__main__":
-    # model = load_model("./models/model_baseline.h5")
+    X_train, y_train, X_val, y_val, X_test, y_test = load_data()
 
-    # X_train, y_train, X_val, y_val, X_test, y_test = load_data()
+    # Evaluate the model
+    model = load_model("./models/model_baseline.h5", custom_objects={"DecayingLRSchedule": DecayingLRSchedule})
+    y_pred = model.predict(X_test)
 
-    # # Evaluate the model
-    # y_pred = model.predict(X_test)
-    # y_pred = np.argmax(y_pred, axis=1)  # Predictions
-    # y_test = np.argmax(y_test, axis=1)  # Ground truth
+    y_pred = np.argmax(y_pred, axis=1)  # Predictions
+    y_test = np.argmax(y_test, axis=1)  # Ground truth
 
     # Plot confusion matrix
-    # plot_confusion_matrix(y_test, y_pred)
+    plot_confusion_matrix(y_test, y_pred)
 
     # Plot LR schedule
-    plot_learning_rate_schedule(epochs=15, learning_rate=0.001, batch_size=64, total_size=48_000)
+    plot_learning_rate_schedule(epochs=15, learning_rate=0.01, batch_size=64, total_size=48_000)
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import (AveragePooling2D, Conv2D, Dense, Dropout,
                                      Flatten, MaxPooling2D)
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 
 
 def make_model(kernel_size: int = 3, pool_size: int = 2, pooling: str = "max", dropout_value: float = None, conv_act: str = "relu"):
@@ -38,16 +39,31 @@ def make_model(kernel_size: int = 3, pool_size: int = 2, pooling: str = "max", d
     return model
 
 
+def prepare_model(model, learning_rate: float = 0.01, batch_size: int = 64, total_size: int = 48_000):
+    # Decrease the learning rate at a 1/2 of the value every 5 epochs
+    learning_rate_schedule = DecayingLRSchedule(learning_rate, batch_size=batch_size, total_size=total_size)
+    opt = Adam(learning_rate=learning_rate_schedule)
+
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+
 class DecayingLRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, lr, batch_size, total_size) -> None:
         super().__init__()
         self.lr = lr
-        self.n_steps = total_size // batch_size  # Number of steps per epoch, e.g. number of batches per epoch
+        self.batch_size = batch_size
+        self.total_size = total_size
+
+        self.n_steps = self.total_size // self.batch_size  # Number of steps per epoch, e.g. number of batches per epoch
 
     def __call__(self, step):
         """Decrease the learning rate at a 1/2 of the value every 5 epochs"""
         epoch = step / self.n_steps  # Current epoch, e.g. epoch 3 or epoch 3.5
         return self.lr * tf.math.pow(0.5, tf.math.floor(epoch / 5))
+
+    def get_config(self):
+        return {"lr": self.lr, "batch_size": self.batch_size, "total_size": self.total_size}
 
 
 if __name__ == "__main__":

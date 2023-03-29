5514d814a838ac4c4dd5759db6d1a0e6378f1e00
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-03-29 11:12:36 +0200

    Added loss plot

--- evaluate.py
+++ evaluate.py
@@ -1,23 +1,27 @@
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import plot_model
 
 from data_processing import load_data
 from models import DecayingLRSchedule
 
 
-def plot_confusion_matrix(y_test, y_pred):
+def plot_confusion_matrix(y_test, y_pred, variation: str):
     cm = confusion_matrix(y_test, y_pred)
     plt.figure(figsize=(10, 10))
 
     ax = plt.gca().set_aspect("equal")  # Set aspect ratio to square
     sns.heatmap(cm, annot=True, fmt="d")
 
-    plt.title("Confusion matrix")
+    plt.title(f"Confusion matrix for model: {variation}")
     plt.ylabel("True label")
     plt.xlabel("Predicted label")
+    plt.savefig(f"./img/cm_{variation}.png")
     plt.show()
 
 
@@ -35,7 +39,39 @@ def plot_learning_rate_schedule(epochs: 15, learning_rate: 0.001, batch_size: in
     plt.ylim(0, None)
     plt.xlabel("Epoch")
     plt.ylabel("Learning rate")
+    plt.savefig("./img/learning_rate_schedule.png")
     plt.show()
+
+
+def plot_history(history: dict, variation: str) -> None:
+    """Plot the history of a model."""
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5), sharex=True)
+
+    ax1.plot(history["loss"], label="Loss")
+    ax1.plot(history["val_loss"], label="Validation loss")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss")
+    ax1.set_xticks(np.arange(0, len(history["loss"]), 1))
+    ax1.legend()
+
+    ax2.plot(history["accuracy"], label="Accuracy")
+    ax2.plot(history["val_accuracy"], label="Validation accuracy")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Accuracy")
+    ax2.set_xticks(np.arange(0, len(history["accuracy"]), 1))
+    ax2.legend()
+
+    plt.suptitle(f"Model: {variation}")
+
+    ax1.yaxis.grid(True)  # Gridlines, only in the horizontal direction
+    ax2.yaxis.grid(True)
+
+    ax1.set_ylim(0, None)  # Loss has no upper limit
+    ax2.set_ylim(0.5, 1)
+
+    plt.show()
+
+    fig.savefig(f"./img/plot_{variation}.png")
 
 
 if __name__ == "__main__":
@@ -43,15 +79,24 @@ if __name__ == "__main__":
     model_variation = "baseline"  # Choose from: {baseline, nike, collegedropout, gigakernel, averagejoe}
     model = load_model(f"./models/model_{model_variation}.h5", custom_objects={"DecayingLRSchedule": DecayingLRSchedule})
 
-    # Load the data
+    # Load the history
+    with open(f"./history/history_{model_variation}.pkl", "rb") as f:
+        model_history = pickle.load(f)
+
+    # Show the model summary with graphviz
+    plot_model(model, show_shapes=True, show_layer_names=True, to_file=f"./img/summary_{model_variation}.png")
+
+    # Plot loss and accuracy, based on history
+    plot_history(model_history, model_variation)
+
+    # Load the data and plot the confusion matrix
     X_train, y_train, X_val, y_val, X_test, y_test = load_data()
     y_pred = model.predict(X_test)
 
     y_pred = np.argmax(y_pred, axis=1)  # Predictions
     y_test = np.argmax(y_test, axis=1)  # Ground truth
 
-    # Plot confusion matrix
-    plot_confusion_matrix(y_test, y_pred)
+    plot_confusion_matrix(y_test, y_pred, model_variation)
 
-    # Plot LR schedule
-    plot_learning_rate_schedule(epochs=15, learning_rate=0.01, batch_size=64, total_size=48_000)
+    # Plot LR schedule, same for all variations
+    # plot_learning_rate_schedule(epochs=15, learning_rate=0.01, batch_size=64, total_size=48_000)
